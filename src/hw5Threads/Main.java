28d562d9cac9fadcf2cf0bd8d4a87cbdc36e6cf1
badmitry <dmitrij88@bk.ru>
2020-05-30 13:00:41 +0300

    При задании размера в 10 сверял значения массивов после рассчетов, были равны. При этом при значениях 10 второй метод считает дольше, видимо из-за того, что делает копии массивов. На длинных массивах в 2 потока считает примерно в 2 раза быстрее.

--- src/hw5Threads/Main.java
+++ src/hw5Threads/Main.java
@@ -2,4 +2,13 @@ package hw5Threads;
 
 public class Main {
 
+    public static void main(String[] args) throws InterruptedException {
+        MyArray arr = new MyArray();
+        long a = System.currentTimeMillis();
+        arr.countValue();
+        System.out.println("Первый медот считает:" + (System.currentTimeMillis() - a));
+        a = System.currentTimeMillis();
+        arr.countValueViaSeparation();
+        System.out.println("Второй медот считает:" + (System.currentTimeMillis() - a));
+    }
 }
